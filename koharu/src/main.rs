4591a9c766a82a6ea7d7a298fdddd77f00a1b12b
Mayo <mayo@linux.com>
2025-10-22 20:33:13 +0900

    Revert "perf: improve image loading time"
    This reverts commit 914c36d47c85aa65eebfdf2c2cefd3f6de228944.

--- koharu/src/main.rs
+++ koharu/src/main.rs
@@ -6,13 +6,11 @@ use lama::Lama;
 use manga_ocr::MangaOCR;
 use ort::execution_providers::CUDAExecutionProvider;
 use rfd::{FileDialog, MessageDialog, MessageLevel};
-use slint::{SharedPixelBuffer, VecModel};
-use tokio::fs;
+use slint::VecModel;
 
 slint::include_modules!();
 
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
+fn main() -> anyhow::Result<()> {
     std::panic::set_hook(Box::new(|info| {
         let msg = info.to_string();
         MessageDialog::new()
@@ -35,56 +33,40 @@ async fn main() -> anyhow::Result<()> {
     let logic = app.global::<Logic>();
 
     logic.on_open_file(move || {
-        let weak = weak.clone();
-        slint::spawn_local(async move {
-            let files = FileDialog::new()
-                .add_filter("images", &["png", "jpg", "jpeg", "webp"])
-                .pick_files();
-
-            if let Some(files) = files {
-                let tasks: Vec<_> = files
-                    .iter()
-                    .map(|path| {
-                        let path = path.clone();
-                        tokio::spawn(async move {
-                            let data = fs::read(&path).await.ok()?;
-                            let img = image::load_from_memory(&data).ok()?;
-                            Some((path, img))
+        let files = FileDialog::new()
+            .add_filter("images", &["png", "jpg", "jpeg", "webp"])
+            .pick_files();
+        if let Some(files) = files {
+            let images: Vec<Image> = files
+                .into_iter()
+                .filter_map(|path| {
+                    slint::Image::load_from_path(&path)
+                        .map(|img| {
+                            let size = img.size();
+                            Image {
+                                source: img,
+                                width: size.width as i32,
+                                height: size.height as i32,
+                                path: path.to_string_lossy().to_string().into(),
+                                name: path
+                                    .file_name()
+                                    .unwrap_or_default()
+                                    .to_string_lossy()
+                                    .to_string()
+                                    .split(".")
+                                    .next()
+                                    .unwrap_or_default()
+                                    .into(),
+                            }
                         })
-                    })
-                    .collect();
-
-                let mut images = Vec::new();
-                for task in tasks {
-                    if let Ok(Some((path, img))) = task.await {
-                        images.push(Image {
-                            source: slint::Image::from_rgba8(
-                                SharedPixelBuffer::<slint::Rgba8Pixel>::clone_from_slice(
-                                    img.to_rgba8().as_raw(),
-                                    img.width(),
-                                    img.height(),
-                                ),
-                            ),
-                            width: img.width() as i32,
-                            height: img.height() as i32,
-                            path: path.to_string_lossy().to_string().into(),
-                            name: path
-                                .file_stem()
-                                .unwrap_or_default()
-                                .to_string_lossy()
-                                .to_string()
-                                .into(),
-                        });
-                    }
-                }
-
-                weak.upgrade()
-                    .expect("Failed to upgrade weak reference")
-                    .global::<State>()
-                    .set_images(VecModel::from_slice(&images));
-            }
-        })
-        .unwrap();
+                        .ok()
+                })
+                .collect();
+            weak.upgrade()
+                .expect("Failed to upgrade weak reference")
+                .global::<State>()
+                .set_images(VecModel::from_slice(&images));
+        }
     });
     logic.on_open_external(|path| open::that(path).expect("Failed to open path"));
 
