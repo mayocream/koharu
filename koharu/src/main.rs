4826341f3c88051247c0505ddfa7ee9d2401ca05
Mayo <mayo@linux.com>
2025-10-24 04:28:47 +0900

    refactor: single page editor

--- koharu/src/main.rs
+++ koharu/src/main.rs
@@ -29,48 +29,43 @@ fn main() -> anyhow::Result<()> {
     let _lama = Lama::new()?;
 
     let app = App::new()?;
-    let weak = app.as_weak();
     let logic = app.global::<Logic>();
 
     logic.on_open_file(move || {
         let files = FileDialog::new()
             .add_filter("images", &["png", "jpg", "jpeg", "webp"])
-            .pick_files();
-        if let Some(files) = files {
-            let mut images: Vec<Image> = files
-                .into_iter()
-                .filter_map(|path| {
-                    slint::Image::load_from_path(&path)
-                        .map(|img| {
-                            let size = img.size();
-                            Image {
-                                source: img,
-                                width: size.width as i32,
-                                height: size.height as i32,
-                                path: path.to_string_lossy().to_string().into(),
-                                name: path
-                                    .file_name()
-                                    .unwrap_or_default()
-                                    .to_string_lossy()
-                                    .to_string()
-                                    .split(".")
-                                    .next()
-                                    .unwrap_or_default()
-                                    .into(),
-                            }
-                        })
-                        .ok()
-                })
-                .collect();
+            .pick_files()
+            .expect("No files selected");
+        let mut images: Vec<Image> = files
+            .into_iter()
+            .filter_map(|path| {
+                slint::Image::load_from_path(&path)
+                    .map(|img| {
+                        let size = img.size();
+                        Image {
+                            source: img,
+                            width: size.width as i32,
+                            height: size.height as i32,
+                            path: path.to_string_lossy().to_string().into(),
+                            name: path
+                                .file_name()
+                                .unwrap_or_default()
+                                .to_string_lossy()
+                                .to_string()
+                                .split(".")
+                                .next()
+                                .unwrap_or_default()
+                                .into(),
+                        }
+                    })
+                    .ok()
+            })
+            .collect();
 
-            // Sort images by name
-            images.sort_by_key(|image| image.name.clone());
+        // Sort images by name
+        images.sort_by_key(|image| image.name.clone());
 
-            weak.upgrade()
-                .expect("Failed to upgrade weak reference")
-                .global::<State>()
-                .set_images(VecModel::from_slice(&images));
-        }
+        VecModel::from_slice(&images)
     });
     logic.on_open_external(|path| open::that(path).expect("Failed to open path"));
 
