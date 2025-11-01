7b155725b87064f9e0287152329aedb9d19aa56f
Mayo <mayo@linux.com>
2025-11-01 12:48:01 +0900

    refactor: add initial next.js

---
+++ ui/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from 'next'
+import { Noto_Sans_JP } from 'next/font/google'
+import './globals.css'
+
+const notoSansJP = Noto_Sans_JP({
+  subsets: ['latin'],
+})
+
+export const metadata: Metadata = {
+  title: 'Koharu',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang='en'>
+      <body className={`${notoSansJP.className} antialiased`}>{children}</body>
+    </html>
+  )
+}
