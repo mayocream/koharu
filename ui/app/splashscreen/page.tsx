f7b71718e3027027728433f496d764fa068785fd
Mayo <mayo@linux.com>
2025-11-03 17:05:12 +0900

    fix: centralize SplashScreen

--- ui/app/splashscreen/page.tsx
+++ ui/app/splashscreen/page.tsx
@@ -1,7 +1,7 @@
 export default function SplashScreen() {
   return (
-    <main className='flex min-h-0 flex-1 flex-col items-center justify-center bg-white'>
-      <span className='text-2xl font-semibold text-pink-800'>Koharu</span>
+    <main className='flex min-h-screen flex-col items-center justify-center bg-white select-none'>
+      <span className='text-2xl font-semibold text-pink-600'>Koharu</span>
       <span className='mt-2 text-lg text-pink-600'>Initializing...</span>
     </main>
   )
