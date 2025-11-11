172e06faead8401628157cfc89fadc0932ec5bb6
Mayo <mayo@linux.com>
2025-11-12 01:23:24 +0900

    chore: rename koharu-app to koharu

--- koharu/src/main.rs
+++ koharu/src/main.rs
@@ -1,7 +1,7 @@
 // Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use koharu_app::app;
+use koharu::app;
 
 fn main() -> anyhow::Result<()> {
     Ok(app::run()?)
