3675905467e3455c3b39cdd8ac1d500946146411
Mayo Takanashi <mayo@linux.com>
2025-12-14 03:33:00 +0900

    Refactor comic_text_detector test for async usage

--- koharu-ml/tests/comic_text_detector.rs
+++ koharu-ml/tests/comic_text_detector.rs
@@ -2,22 +2,15 @@ use std::path::Path;
 
 use koharu_ml::{comic_text_detector::ComicTextDetector};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use tracing::instrument;
+#[tokio::test]
+async fn comic_text_detector() -> anyhow::Result<()> {
+    let model = ComicTextDetector::load(false).await?;
 
-    #[tokio::test]
-    #[instrument(level = "debug", skip_all)]
-    async fn comic_text_detector() -> anyhow::Result<()> {
-        let model = ComicTextDetector::load(false).await?;
+    let img = image::open(Path::new(env!("CARGO_MANIFEST_DIR")).join("tests/fixtures/1.jpg"))?;
+    let (boxes, mask) = model.inference(&img)?;
 
-        let img = image::open(Path::new(env!("CARGO_MANIFEST_DIR")).join("tests/fixtures/1.jpg"))?;
-        let (boxes, mask) = model.inference(&img)?;
+    assert!(!boxes.is_empty());
+    assert!(mask.iter().any(|&v| v > 0u8));
 
-        assert!(!boxes.is_empty());
-        assert!(mask.iter().any(|&v| v > 0u8));
-
-        Ok(())
-    }
+    Ok(())
 }
