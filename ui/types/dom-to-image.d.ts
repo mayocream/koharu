edf1142b92c5126a672048edb73f43fc1ff2a1f4
Mayo <mayo@linux.com>
2026-02-02 02:45:07 +0900

    chore: remove unused types

--- ui/types/dom-to-image.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'dom-to-image' {
-  const api: {
-    toPng(node: HTMLElement, options?: Record<string, unknown>): Promise<string>
-    toSvg(node: HTMLElement, options?: Record<string, unknown>): Promise<string>
-  }
-  export = api
-}
