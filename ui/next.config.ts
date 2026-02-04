c48ed3b4b3b80069d1e0868a0f1cf6fd079b6ff8
Mayo <mayo@linux.com>
2026-02-04 21:56:24 +0900

    chore: remove unused settings

--- ui/next.config.ts
+++ ui/next.config.ts
@@ -1,8 +1,5 @@
 import type { NextConfig } from 'next'
 
-const isProd = process.env.NODE_ENV === 'production'
-const internalHost = process.env.TAURI_DEV_HOST || 'localhost'
-
 const nextConfig: NextConfig = {
   reactCompiler: true,
   devIndicators: false,
@@ -10,8 +7,6 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  // https://v2.tauri.app/start/frontend/nextjs/#update-nextjs-configuration
-  assetPrefix: isProd ? undefined : `http://${internalHost}:3000`,
 }
 
 export default nextConfig
