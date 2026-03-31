1d1b3df09d49874584aa8e8cb42f0d2796c918b1
Dev_Nergis <136839304+DevNergis@users.noreply.github.com>
2026-04-01 05:43:34 +0900

    feat: add Korean (ko-KR) locale and translate (#316)

--- ui/lib/i18n.ts
+++ ui/lib/i18n.ts
@@ -12,6 +12,7 @@ import jaJP from '@/public/locales/ja-JP/translation.json'
 import ruRU from '@/public/locales/ru-RU/translation.json'
 import esES from '@/public/locales/es-ES/translation.json'
 import trTR from '@/public/locales/tr-TR/translation.json'
+import koKR from '@/public/locales/ko-KR/translation.json'
 
 export const resources = {
   'en-US': { translation: enUS },
@@ -21,6 +22,7 @@ export const resources = {
   'ru-RU': { translation: ruRU },
   'es-ES': { translation: esES },
   'tr-TR': { translation: trTR },
+  'ko-KR': { translation: koKR },
 } satisfies Resource
 
 export const supportedLanguages = Object.keys(resources)
